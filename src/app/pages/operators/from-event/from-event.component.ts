c8c0b7a0917261594715def087e4c8e81205b1e5
florian935 <florian.martin63000@gmail.com>
2021-10-02 12:12:39 +0200

    Feat: added another demo for fromEvent operator

--- src/app/pages/operators/from-event/from-event.component.ts
+++ src/app/pages/operators/from-event/from-event.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { fromEvent, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'app-from-event',
@@ -8,14 +8,53 @@ import { map } from 'rxjs/operators';
     styleUrls: ['./from-event.component.scss'],
 })
 export class FromEventComponent implements AfterViewInit {
-    clientXTracker$?: Observable<number>;
+    clickClientXTracker$?: Observable<number>;
+    clickClientXTrackerArray: Array<number> = [];
+    wheelClientXTracker$?: Observable<number>;
+    wheelClientXTrackerArray: Array<number> = [];
+    mouseenterClientXTracker$?: Observable<number>;
+    mouseenterClientXTrackerArray: Array<number> = [];
 
     constructor() {}
 
     ngAfterViewInit(): void {
-        this.clientXTracker$ = fromEvent(
+        this.clickClientXTracker$ = fromEvent(
             document.getElementById('click-button')!,
             'click'
-        ).pipe(map((event: Event) => (event as PointerEvent).clientX));
+        ).pipe(
+            map((event: Event) => (event as PointerEvent).clientX),
+            tap((clientX: number) => {
+                this.clickClientXTrackerArray = [
+                    ...this.clickClientXTrackerArray,
+                    clientX,
+                ];
+            })
+        );
+
+        this.wheelClientXTracker$ = fromEvent(
+            document.getElementById('wheel-button')!,
+            'wheel'
+        ).pipe(
+            map((event: Event) => (event as WheelEvent).clientX),
+            tap((clientX: number) => {
+                this.wheelClientXTrackerArray = [
+                    ...this.wheelClientXTrackerArray,
+                    clientX,
+                ];
+            })
+        );
+
+        this.mouseenterClientXTracker$ = fromEvent(
+            document.getElementById('mouseenter-button')!,
+            'mouseenter'
+        ).pipe(
+            map((event: Event) => (event as MouseEvent).clientX),
+            tap((clientX: number) => {
+                this.mouseenterClientXTrackerArray = [
+                    ...this.mouseenterClientXTrackerArray,
+                    clientX,
+                ];
+            })
+        );
     }
 }
