bee1bfaefe05ca2b8ec949ba2cdeac3c36534963
florian935 <florian.martin63000@gmail.com>
2022-04-13 20:38:20 +0200

    Feat: complete demo for forkJoin and zipWith operators

--- src/app/pages/operators/zip-with/zip-with.component.ts
+++ src/app/pages/operators/zip-with/zip-with.component.ts
@@ -12,8 +12,8 @@ export class ZipWithComponent implements OnInit {
 
     ngOnInit(): void {
         const source$ = interval(1000);
-        const source2$ = of(1000, 1000, 1000);
-        const source3$ = of(2000, 2000, 2000);
+        const source2$ = interval(2000);
+        const source3$ = interval(3000);
 
         source$
             .pipe(zipWith(source2$, source3$))
